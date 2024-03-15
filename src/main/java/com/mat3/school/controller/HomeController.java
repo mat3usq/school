bf9f583d7c58329db77e4d3c40566742a5cfa042
mat3usq <mmogielnicki123@gmail.com>
2024-03-15 15:42:31 +0100

    add username to home page

--- src/main/java/com/mat3/school/controller/HomeController.java
+++ src/main/java/com/mat3/school/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.mat3.school.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
-    @RequestMapping("/home")
-    public String displayHomePage() {
+    @RequestMapping(value = {"", "/", "/home"})
+    public String displayHomePage(Model model) {
+        model.addAttribute("username", "Mateo");
         return "home.html";
     }
 
