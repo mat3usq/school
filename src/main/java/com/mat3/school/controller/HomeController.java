916c187ccba8de5bd4d0efcd40b9dcaecc85da99
mat3usq <mmogielnicki123@gmail.com>
2024-03-15 23:43:20 +0100

    add frontend home.html

--- src/main/java/com/mat3/school/controller/HomeController.java
+++ src/main/java/com/mat3/school/controller/HomeController.java
@@ -1,14 +1,12 @@
 package com.mat3.school.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomePage(Model model) {
-        model.addAttribute("username", "Mateo");
+    public String displayHomePage() {
         return "home.html";
     }
 
