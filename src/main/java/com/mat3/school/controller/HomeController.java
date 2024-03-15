4d41e6276026894ef8ed68ad2cc5a50b0cc93229
mat3usq <mmogielnicki123@gmail.com>
2024-03-15 14:54:44 +0100

    Initial commit

---
+++ src/main/java/com/mat3/school/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.mat3.school.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String displayHomePage() {
+        return "home.html";
+    }
+
+}
