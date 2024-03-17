b42f7795638340937374a639289342dfa4a9255a
mat3usq <mmogielnicki123@gmail.com>
2024-03-18 00:20:06 +0100

    update ContactController

--- src/main/java/com/mat3/school/controller/ContactController.java
+++ src/main/java/com/mat3/school/controller/ContactController.java
@@ -1,13 +1,33 @@
 package com.mat3.school.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class ContactController {
+
+    public static final Logger log = LoggerFactory.getLogger(ContactController.class);
+
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
     }
 
+    @RequestMapping(value = "/saveMsg",method = POST)
+    public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum,
+                                    @RequestParam String email, @RequestParam String subject, @RequestParam String message) {
+        log.info("Name : " + name);
+        log.info("Mobile Number : " + mobileNum);
+        log.info("Email Address : " + email);
+        log.info("Subject : " + subject);
+        log.info("Message : " + message);
+        log.info("Email was send!");
+        return new ModelAndView("redirect:/contact");
+    }
 }
