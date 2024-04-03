f02a5c90897b4bacb310ce475a163136e86dfe4e
mat3usq <mmogielnicki123@gmail.com>
2024-04-03 11:57:12 +0200

    added classes view

--- src/main/java/com/mat3/school/controller/AdminController.java
+++ src/main/java/com/mat3/school/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.mat3.school.controller;
 
+import com.mat3.school.model.SchoolClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ public class AdminController {
     @RequestMapping("/displayClasses")
     public ModelAndView displayClasses() {
         ModelAndView modelAndView = new ModelAndView("classes.html");
+        modelAndView.addObject("schoolClass", new SchoolClass());
         return modelAndView;
     }
 }
