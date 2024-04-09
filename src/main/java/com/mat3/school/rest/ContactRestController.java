2d1b29b7ecd21d3fa86344420d7a09edcdf327d3
mat3usq <mmogielnicki123@gmail.com>
2024-04-10 00:05:46 +0200

    save and delete messages by restController

--- src/main/java/com/mat3/school/rest/ContactRestController.java
+++ src/main/java/com/mat3/school/rest/ContactRestController.java
@@ -1,9 +1,15 @@
 package com.mat3.school.rest;
 
 import com.mat3.school.model.Contact;
+import com.mat3.school.model.Response;
 import com.mat3.school.repository.ContactRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,17 +26,43 @@ public class ContactRestController {
     }
 
     @GetMapping("/getMessagesByStatus")
-    @ResponseBody
     public List<Contact> getMessagesByStatus(@RequestParam(name = "status") String status) {
         return contactRepository.findByStatus(status);
     }
 
     @GetMapping("/getAllMsgsByStatus")
-    @ResponseBody
     public List<Contact> getAllMsgsByStatus(@RequestBody Contact contact) {
         if (null != contact && null != contact.getStatus())
             return contactRepository.findByStatus(contact.getStatus());
         else
             return List.of();
+    }
+
+    @PostMapping("/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestHeader("invocationFrom") String invocationFrom, @Valid @RequestBody Contact contact) {
+        log.info(String.format("Header invocationFrom = %s", invocationFrom));
+        contactRepository.save(contact);
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message saved successfully");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("isMsgSaved", "true")
+                .body(response);
+    }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) -> log.info(String.format("Header '%s' = %s", key,
+                String.join("|", value))));
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 }
