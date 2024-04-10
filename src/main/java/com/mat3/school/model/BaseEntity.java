fa12f62023ca56f1542f589461b4c01fa702406a
mat3usq <mmogielnicki123@gmail.com>
2024-04-10 18:33:00 +0200

    parsing return values from rest controller by Jacson

--- src/main/java/com/mat3/school/model/BaseEntity.java
+++ src/main/java/com/mat3/school/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.mat3.school.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
@@ -18,17 +19,21 @@ import java.time.LocalDateTime;
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
+    @JsonIgnore
     private LocalDateTime createdAt;
 
     @CreatedBy
     @Column(updatable = false)
+    @JsonIgnore
     private String createdBy;
 
     @LastModifiedDate
     @Column(insertable = false)
+    @JsonIgnore
     private LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(insertable = false)
+    @JsonIgnore
     private String updatedBy;
 }
