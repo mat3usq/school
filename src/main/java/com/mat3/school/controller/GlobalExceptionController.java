14a4f88988c65a3e8a7d9f82a4951f8bc0c1d3cd
mat3usq <mmogielnicki123@gmail.com>
2024-04-10 17:35:01 +0200

    adding GlobalExceptionRestController

--- src/main/java/com/mat3/school/controller/GlobalExceptionController.java
+++ src/main/java/com/mat3/school/controller/GlobalExceptionController.java
@@ -1,6 +1,7 @@
 package com.mat3.school.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,18 +12,25 @@ exceptions across the whole application in one global handling component. It can
 as an interceptor of exceptions thrown by methods annotated with @RequestMapping and similar.
 * */
 @Slf4j
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class GlobalExceptionController {
     /*
     @ExceptionHandler will register the given method for a given
     exception type, so that ControllerAdvice can invoke this method
     logic if a given exception type is thrown inside the web application.
     * */
-    @ExceptionHandler(Exception.class)
-    public ModelAndView exceptionHandler(Exception exception){
+    @ExceptionHandler({Exception.class})
+    public ModelAndView exceptionHandler(Exception exception) {
+        String errorMsg;
         ModelAndView errorPage = new ModelAndView();
         errorPage.setViewName("error");
-        errorPage.addObject("errormsg", exception.getMessage());
+        if (exception.getMessage() != null)
+            errorMsg = exception.getMessage();
+        else if (exception.getCause() != null)
+            errorMsg = exception.getCause().toString();
+        else
+            errorMsg = exception.toString();
+        errorPage.addObject("errormsg", errorMsg);
         return errorPage;
     }
 }
