7b347a98271aa1e86c21f3406a91f3dc3bf1642a
mat3usq <mmogielnicki123@gmail.com>
2024-04-10 12:43:00 +0200

    adding closing Msg in rest Controller

--- src/main/java/com/mat3/school/rest/ContactRestController.java
+++ src/main/java/com/mat3/school/rest/ContactRestController.java
@@ -1,5 +1,6 @@
 package com.mat3.school.rest;
 
+import com.mat3.school.constants.SchoolConstants;
 import com.mat3.school.model.Contact;
 import com.mat3.school.model.Response;
 import com.mat3.school.repository.ContactRepository;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RestController
@@ -61,6 +63,27 @@ public class ContactRestController {
         Response response = new Response();
         response.setStatusCode("200");
         response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
+    }
+
+    @PatchMapping("/closeMsg")
+    public ResponseEntity<Response> closeMsg(@RequestBody Contact contactReq) {
+        Response response = new Response();
+        Optional<Contact> contact = contactRepository.findById(contactReq.getContactId());
+        if (contact.isPresent()) {
+            contact.get().setStatus(SchoolConstants.CLOSE);
+            contactRepository.save(contact.get());
+        } else {
+            response.setStatusCode("400");
+            response.setStatusMsg("Invalid Contact ID received");
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(response);
+        }
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully closed");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(response);
